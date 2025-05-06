e41564d1556aee0be4a3e815f94dee2e29fcf9e8
ARMAAN-SAHOO <armaansahoo6@gmail.com>
2025-05-06 07:59:44 +0530

    main

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/ARMAAN-SAHOO/portfolio',
+  base: '/',  // Use the relative path to your repo
   plugins: [react()],
   css: {
     modules: {
